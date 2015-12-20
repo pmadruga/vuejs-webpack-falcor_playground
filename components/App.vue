50f12312942f7b800ed26f2dd9e741e2561b1eee
Pedro Madruga <pmadruga@gmail.com>
2015-12-20 16:05:34 +0100

    integration with falcor (work in progress)

---
+++ components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="example">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  data () {
+  	var model = new falcor.Model({source: new falcor.HttpDataSource('/model.json') });
+  	model.get("greeting").then(function(response) {
+          console.log(response.json.greeting);
+        });
+    return {
+      msg: 'This is the app main component.'
+    }
+  }
+}
+</script>
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // main.js
 var Vue = require('vue')
   // require a *.vue component
-  //var App = require('./components/App.vue')
+var AppComponent = require('./components/App.vue')
 
 // mount a root Vue instance
 new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!'
+  el: 'body',
+  components: {
+    app: AppComponent
   }
 })
